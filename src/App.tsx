43c5a1c3f9a5ac591a4198aa83894c6931acec89
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2025-07-23 08:37:32 +0530

    removed ununsed css files import comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-// import "./App.css";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Dashboard from "./routes/Dashboard";
@@ -7,16 +5,16 @@ import StudentList from "./routes/StudentList";
 import StudentProfile from "./routes/StudentProfile";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/students" element={<StudentList />} />
-        <Route path="/student/:id" element={<StudentProfile />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+				<Route path="/students" element={<StudentList />} />
+				<Route path="/student/:id" element={<StudentProfile />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
