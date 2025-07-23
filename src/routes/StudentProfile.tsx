4a9b4ffe87941d90f51c76b72c5af35fd0fd2ea9
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2025-07-23 08:33:23 +0530

    changed back redirection from dashboard to student lists page

--- src/routes/StudentProfile.tsx
+++ src/routes/StudentProfile.tsx
@@ -4,61 +4,68 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const StudentProfile = () => {
-  const { id } = useParams();
-  const [student, setStudent] = useState(null);
-  const navigate = useNavigate();
+	const { id } = useParams();
+	const [student, setStudent] = useState(null);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:3000/api/students/${id}`)
-      .then((res) => setStudent(res.data))
-      .catch((err) => {
-        console.log("error occuurerdd: ", err);
-        navigate("/");
-      });
-  }, [id, navigate]);
+	useEffect(() => {
+		axios
+			.get(`http://localhost:3000/api/students/${id}`)
+			.then((res) => setStudent(res.data))
+			.catch((err) => {
+				console.log("error occuurerdd: ", err);
+				navigate("/");
+			});
+	}, [id, navigate]);
 
-  if (!student) return <Typography p={3}>Loading...</Typography>;
+	if (!student) return <Typography p={3}>Loading...</Typography>;
 
-  return (
-    <Box p={4}>
-      <Paper elevation={3} sx={{ p: 4 }}>
-        <Box display="flex" alignItems="center" gap={3}>
-          <Avatar sx={{ width: 80, height: 80, bgcolor: "primary.main" }}>
-            {student.name.charAt(0)}
-          </Avatar>
-          <Box>
-            <Typography variant="h5">{student.name}</Typography>
-            <Typography variant="subtitle1">
-              Roll: {student.rollNumber}
-            </Typography>
-            <Typography variant="subtitle2">
-              Stream: {student.stream}
-            </Typography>
-            <Typography variant="subtitle3">Grade: {student.grade}</Typography>
-          </Box>
-        </Box>
-        <Divider sx={{ my: 3 }} />
-        <Box>
-          <Typography variant="h6">Parent Details</Typography>
-          <Typography>
-            <strong>Father:</strong> {student.parent.fatherName} (
-            {student.parent.fatherPhone})
-          </Typography>
-          <Typography>
-            <strong>Mother:</strong> {student.parent.motherName} (
-            {student.parent.motherPhone})
-          </Typography>
-        </Box>
+	return (
+		<Box p={4}>
+			<Paper elevation={3} sx={{ p: 4 }}>
+				<Box display="flex" alignItems="center" gap={3}>
+					<Avatar
+						sx={{ width: 80, height: 80, bgcolor: "primary.main" }}
+					>
+						{student.name.charAt(0)}
+					</Avatar>
+					<Box>
+						<Typography variant="h5">{student.name}</Typography>
+						<Typography variant="subtitle1">
+							Roll: {student.rollNumber}
+						</Typography>
+						<Typography variant="subtitle2">
+							Stream: {student.stream}
+						</Typography>
+						<Typography variant="subtitle3">
+							Grade: {student.grade}
+						</Typography>
+					</Box>
+				</Box>
+				<Divider sx={{ my: 3 }} />
+				<Box>
+					<Typography variant="h6">Parent Details</Typography>
+					<Typography>
+						<strong>Father:</strong> {student.parent.fatherName} (
+						{student.parent.fatherPhone})
+					</Typography>
+					<Typography>
+						<strong>Mother:</strong> {student.parent.motherName} (
+						{student.parent.motherPhone})
+					</Typography>
+				</Box>
 
-        <Box mt={3}>
-          <Button variant="contained" onClick={() => navigate("/")}>
-            Back to Dashboard
-          </Button>
-        </Box>
-      </Paper>
-    </Box>
-  );
+				<Box mt={3}>
+					<Button
+						variant="contained"
+						onClick={() => navigate("/students")}
+					>
+						Back to Dashboard
+					</Button>
+				</Box>
+			</Paper>
+		</Box>
+	);
 };
 
 export default StudentProfile;
